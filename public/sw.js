f96eb161a065948957a58e965c021aac46a0f0d7
whitehorse1990629-dotcom <whitehorse1990629@gmail.com>
2026-08-23 01:25:15 +0900

    Implement service worker for caching assets

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,39 @@
+const CACHE_NAME = "trainingapp-v4.2";
 
+const APP_SHELL = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(APP_SHELL);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
